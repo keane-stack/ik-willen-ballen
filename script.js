04e59f4defc625e18031f7ceeb22c7f9224d02ac
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-09-15 12:12:54 +0000

    tg

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-class Ellipse {
-  constructor(x, y, w, h, vx, vy, color) {
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.vx = vx;
-      this.vy = vy
-      this.color = color
-  }
-
-  drawEllipse(){
-    fill(this.color)
-    ellipse(this.x, this.y, this.w, this.h);
-
-
-    this.y = this.y + this.vy;
-    this.x = this.x + this.vx;
-
-
-    if(this.x <=0 || this.x >=500){
-      this.vx = this.vx * -1;
-    }
-
-    if(this.y <=0 || this.y >=500){
-      this.vy = this.vy * -1;
-
-    }
-  }
-}
-var ballList = [ 
-  ball1 = new Ellipse(60,10,30,30,5,5,"white");
-  ball2 = new Ellipse(10,100,30,30,5,5,"yellow");
-  ball3 = new Ellipse(10,10,30,30,5,5,"red");
-  ball4 = new Ellipse(50,10,30,30,5,5,"green");
-  ball5 = new Ellipse(1,10,30,30,5,5,"orange");
-  ball6 = new Ellipse(20,10,30,30,5,5,"orange");
-  ]
-
-function setup(){
-  createCanvas(500, 500);
-
-
+class Ball{
+constructor(x, y, w, h, vx, vy,_color){
+this.x = x;
+this.y = y;
+this.width = w;
+this.height = h;
+this.vx = vx;
+this.vy = vy;
+this.color = _color
 }
 
-function draw(){
-  background(225);
-  ballList;
+draw() {
+fill(this.color)
+ellipse (this.x,this.y,50,50);
+this.x = this.x + this.vx;
+this.y = this.y + this.vy;
+
+if(this.x > 500 || this.x < 0){
+this.vx = this.vx * -1;
+}
+
+if(this.y < 0 || this.y > 400){
+this.vy = this.vy * -1;
+}
+}
+}
+
+var ball1, ball2, ball3, ball4, ball5, ball6, ball7, ball8, ball9, ball10, ball11, ball12;
+var ballList = [];
+
+
+function setup() {
+createCanvas(500, 400);
+for(var i = 0; i < 50; i++){
+var r = Math.floor(Math.random() * 6) + 1;
+var x = Math.floor(Math.random() * 500) + 1;
+var y = Math.floor(Math.random() * 500) + 1;
+var c = Math.floor(Math.random() * 255) + 1;
+var l = Math.floor(Math.random() * 80) + 1;
+ball = new Ball(x,y,l,50,r,r,c);
+ballList.push(ball);
+}
 
 }
- 
+
+function draw() {
+background(255);
+ballList.forEach(function(ball){
+ball.draw();
+});
+}
