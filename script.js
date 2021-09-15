87998a8b537f50f4c163e1d718dc5ffc5eb39dd2
keanestack <74552264+keane-stack@users.noreply.github.com>
2021-09-15 12:09:39 +0000

    d

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-class Ball{
-   constructor(x,y,h,w,vx,vy) {
-    this.h = h;
-    this.w = w;
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
+class Ellipse {
+  constructor(x, y, w, h, vx, vy, color) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.vx = vx;
+      this.vy = vy
+      this.color = color
+  }
+
+  drawEllipse(){
+    fill(this.color)
+    ellipse(this.x, this.y, this.w, this.h);
+
+
+    this.y = this.y + this.vy;
+    this.x = this.x + this.vx;
+
+
+    if(this.x <=0 || this.x >=500){
+      this.vx = this.vx * -1;
+    }
+
+    if(this.y <=0 || this.y >=500){
+      this.vy = this.vy * -1;
+
+    }
   }
 }
+var ballList = [ 
+  ball1 = new Ellipse(60,10,30,30,5,5,"white");
+  ball2 = new Ellipse(10,100,30,30,5,5,"yellow");
+  ball3 = new Ellipse(10,10,30,30,5,5,"red");
+  ball4 = new Ellipse(50,10,30,30,5,5,"green");
+  ball5 = new Ellipse(1,10,30,30,5,5,"orange");
+  ball6 = new Ellipse(20,10,30,30,5,5,"orange");
+  ]
 
-var x, y, v;
+function setup(){
+  createCanvas(500, 500);
 
-function setup() {
-	createCanvas(500, 500);
 
-  x = 0;
-  y = 280;
-  vx = 6;
-  vy = 6;
+}
+
+function draw(){
+  background(225);
+  ballList;
+
+}
  
-
-}
-
-function draw() {
-	background(225);
-  
-  ellipse(x,y,50,50);
-  x = x + vx;
-  y = y + vy;
-
-
-
-  if(x <= 0 || x >= 500){
-    vx = vx * -1;
-  }
-
-  if(y <= 0 || y >= 500){ 
-    vy = vy * -1;
-  }
- 
-}
